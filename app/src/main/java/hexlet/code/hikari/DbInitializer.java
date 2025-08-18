e61bae484b96810f17cc90def72b48821d5e5ea9
Asya-67 <annadorovskikh111@gmail.com>
2025-08-18 16:35:30 +0300

    todays2

--- app/src/main/java/hexlet/code/hikari/DbInitializer.java
+++ app/src/main/java/hexlet/code/hikari/DbInitializer.java
@@ -6,16 +6,21 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DbInitializer {
-    public static void init(DataSource dataSource) throws SQLException {
+    public static void init(DataSource dataSource) {
         try (Connection conn = dataSource.getConnection();
              Statement stmt = conn.createStatement()) {
-            stmt.executeUpdate("""
+
+            String sql = """
                 CREATE TABLE IF NOT EXISTS urls (
                     id BIGSERIAL PRIMARY KEY,
                     name VARCHAR(255) NOT NULL,
                     created_at TIMESTAMP NOT NULL
-                )
-            """);
+                );
+                """;
+            stmt.execute(sql);
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to initialize database", e);
         }
     }
 }
