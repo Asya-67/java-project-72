8273ed14745597cbca979488783240224a7840bd
Asya-67 <annadorovskikh111@gmail.com>
2025-08-31 20:40:33 +0300

    31 08 6

--- app/jte-classes/gg/jte/generated/ondemand/JteurlGenerated.java
+++ app/jte-classes/gg/jte/generated/ondemand/JteurlGenerated.java
@@ -1,16 +1,17 @@
 package gg.jte.generated.ondemand;
 import hexlet.code.dto.UrlDto;
+import hexlet.code.models.UrlCheck;
 public final class JteurlGenerated {
 	public static final String JTE_NAME = "url.jte";
-	public static final int[] JTE_LINE_INFO = {0,0,1,1,1,6,6,6,6,19,19,20,20,22,22,22,23,23,23,24,24,24,25,25,25,26,26,26,27,27,27,29,29,30,30,34,34,40,40,40,1,1,1,1};
+	public static final int[] JTE_LINE_INFO = {0,0,1,2,2,2,7,7,7,7,21,21,22,22,24,24,24,25,25,25,26,26,26,27,27,27,28,28,28,29,29,29,31,31,32,32,36,36,43,43,43,2,2,2,2};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, UrlDto page) {
 		jteOutput.writeContent("\r\n@extends(\"layout.jte\")\r\n@section content {\r\n    <div class=\"container mt-4\">\r\n        <h1>Страница: ");
 		jteOutput.setContext("h1", null);
 		jteOutput.writeUserContent(page.getUrl().getName());
-		jteOutput.writeContent("</h1>\r\n        <table class=\"table table-bordered mt-3\">\r\n            <thead>\r\n                <tr>\r\n                    <th>ID</th>\r\n                    <th>Status Code</th>\r\n                    <th>Title</th>\r\n                    <th>H1</th>\r\n                    <th>Description</th>\r\n                    <th>Created At</th>\r\n                </tr>\r\n            </thead>\r\n            <tbody>\r\n                ");
+		jteOutput.writeContent("</h1>\r\n\r\n        <table class=\"table table-bordered mt-3\">\r\n            <thead>\r\n                <tr>\r\n                    <th>ID</th>\r\n                    <th>Status Code</th>\r\n                    <th>Title</th>\r\n                    <th>H1</th>\r\n                    <th>Description</th>\r\n                    <th>Created At</th>\r\n                </tr>\r\n            </thead>\r\n            <tbody>\r\n                ");
 		if (page.getChecks() != null && !page.getChecks().isEmpty()) {
 			jteOutput.writeContent("\r\n                    ");
-			for (check : page.getChecks()) {
+			for (hexlet.code.models.UrlCheck check : page.getChecks()) {
 				jteOutput.writeContent("\r\n                        <tr>\r\n                            <td>");
 				jteOutput.setContext("td", null);
 				jteOutput.writeUserContent(check.getId());
@@ -28,14 +29,14 @@ public final class JteurlGenerated {
 				jteOutput.writeUserContent(check.getDescription());
 				jteOutput.writeContent("</td>\r\n                            <td>");
 				jteOutput.setContext("td", null);
-				jteOutput.writeUserContent(check.getCreatedAt());
+				jteOutput.writeUserContent(check.getCreatedAt().toLocalDateTime().toString());
 				jteOutput.writeContent("</td>\r\n                        </tr>\r\n                    ");
 			}
 			jteOutput.writeContent("\r\n                ");
 		} else {
 			jteOutput.writeContent("\r\n                    <tr>\r\n                        <td colspan=\"6\" class=\"text-center\">Проверки отсутствуют</td>\r\n                    </tr>\r\n                ");
 		}
-		jteOutput.writeContent("\r\n            </tbody>\r\n        </table>\r\n        <a href=\"/urls\" class=\"btn btn-secondary mt-3\">Назад к списку URL</a>\r\n    </div>\r\n}\r\n");
+		jteOutput.writeContent("\r\n            </tbody>\r\n        </table>\r\n\r\n        <a href=\"/urls\" class=\"btn btn-secondary mt-3\">Назад к списку URL</a>\r\n    </div>\r\n}\r\n");
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
 		UrlDto page = (UrlDto)params.getOrDefault("page", null);
