6c680c1e4d1db240c1b8c4e6fadc79abbfcf6c52
Asya-67 <annadorovskikh111@gmail.com>
2025-09-04 00:58:43 +0300

    04093

--- app/src/main/java/hexlet/code/Database.java
+++ app/src/main/java/hexlet/code/Database.java
@@ -4,8 +4,6 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class Database {
 
@@ -15,60 +13,22 @@ public class Database {
         if (dataSource == null) {
             HikariConfig config = new HikariConfig();
 
-            String jdbcUrl;
-            String username;
-            String password;
-
             String databaseUrl = System.getenv("JDBC_DATABASE_URL");
-            String envUsername = System.getenv("JDBC_DATABASE_USERNAME");
-            String envPassword = System.getenv("JDBC_DATABASE_PASSWORD");
 
             if (databaseUrl != null && !databaseUrl.isBlank()) {
-                try {
-                    if (databaseUrl.startsWith("postgres://")) {
-                        databaseUrl = databaseUrl.replaceFirst("postgres://", "postgresql://");
-                    }
 
-                    URI dbUri = new URI(databaseUrl);
-
-                    String userInfo = dbUri.getUserInfo();
-                    if ((userInfo == null || !userInfo.contains(":")) && (envUsername == null || envPassword == null)) {
-                        throw new RuntimeException("JDBC_DATABASE_URL не содержит username и password, "
-                                + "и переменные окружения не заданы");
-                    }
-
-                    if (userInfo != null && userInfo.contains(":")) {
-                        String[] userParts = userInfo.split(":");
-                        username = userParts[0];
-                        password = userParts[1];
-                    } else {
-                        username = envUsername;
-                        password = envPassword;
-                    }
-
-                    jdbcUrl = "jdbc:postgresql://" + dbUri.getHost()
-                            + ":" + dbUri.getPort()
-                            + dbUri.getPath();
-
-                    Class.forName("org.postgresql.Driver");
-                } catch (URISyntaxException | ClassNotFoundException e) {
-                    throw new RuntimeException("Неправильный JDBC_DATABASE_URL или драйвер PostgreSQL не найден", e);
+                if (!databaseUrl.startsWith("jdbc:")) {
+                    config.setJdbcUrl("jdbc:" + databaseUrl);
+                } else {
+                    config.setJdbcUrl(databaseUrl);
                 }
+
             } else {
 
-                jdbcUrl = "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;MODE=PostgreSQL";
-                username = "sa";
-                password = "";
-                try {
-                    Class.forName("org.h2.Driver");
-                } catch (ClassNotFoundException e) {
-                    throw new RuntimeException("H2 Driver not found", e);
-                }
+                config.setJdbcUrl("jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;MODE=PostgreSQL");
+                config.setUsername("sa");
+                config.setPassword("");
             }
-
-            config.setJdbcUrl(jdbcUrl);
-            config.setUsername(username);
-            config.setPassword(password);
 
             config.setMaximumPoolSize(10);
             config.setMinimumIdle(2);
